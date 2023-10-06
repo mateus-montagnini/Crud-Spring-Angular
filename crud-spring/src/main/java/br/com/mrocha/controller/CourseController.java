0f45ac87aba80159611ad55c8bb45669516b4941
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-06 13:15:08 -0300

    Metodo get no controller

--- crud-spring/src/main/java/br/com/mrocha/controller/CourseController.java
+++ crud-spring/src/main/java/br/com/mrocha/controller/CourseController.java
@@ -1,4 +1,23 @@
 package br.com.mrocha.controller;
 
+import br.com.mrocha.model.Course;
+import br.com.mrocha.repository.ICourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
 public class CourseController {
+
+    @Autowired
+    private ICourseRepository courseRepository;
+
+    @GetMapping
+        public List<Course> list() {
+        return courseRepository.findAll();
+    }
 }
