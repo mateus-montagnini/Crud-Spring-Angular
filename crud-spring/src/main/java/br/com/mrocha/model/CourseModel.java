e1ddbafd893d6f8df7098029be40b419ee1131e0
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-06 13:14:49 -0300

    Extendendo o JpaRepository no ICourse

--- crud-spring/src/main/java/br/com/mrocha/model/CourseModel.java
+++
@@ -1,4 +0,0 @@
-package br.com.mrocha.model;
-
-public class CourseModel {
-}
--- crud-spring/src/main/java/br/com/mrocha/repository/ICourseRepository.java
+++ crud-spring/src/main/java/br/com/mrocha/repository/ICourseRepository.java
@@ -1,4 +1,9 @@
 package br.com.mrocha.repository;
 
-public interface ICourseRepository {
+import br.com.mrocha.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICourseRepository extends JpaRepository<Course, Long> {
 }
