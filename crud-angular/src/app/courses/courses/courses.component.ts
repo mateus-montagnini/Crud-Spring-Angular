600be06d2a920bfa50558f8f82bf6dc7ac78352a
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-07 08:19:52 -0300

    Criando metodo para o botao de adicionar curso

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, Observable, of } from 'rxjs';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 import { Course } from '../model/course';
@@ -19,7 +20,10 @@ export class CoursesComponent implements OnInit {
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+
     ) {
     this.courses$ = this.coursesService.list()
     .pipe(
@@ -37,6 +41,11 @@ export class CoursesComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+  }
+
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route});
 
   }
 }
