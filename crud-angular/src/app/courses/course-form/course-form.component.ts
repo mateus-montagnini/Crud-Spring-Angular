bad14f02d89c3ebee2027c5c63b3392da8c0fe0b
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-09 11:35:47 -0300

    Adicionando caixa de opcoes para escolher o curso

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,7 @@
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -10,7 +12,9 @@ export class CourseFormComponent {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -22,10 +26,16 @@ export class CourseFormComponent {
   }
 
   onSubmit() {
+    this.service.save(this.form.value)
+    .subscribe(result => console.log(result), error => this.onError());
 
   }
 
   onCancel() {
 
+  }
+
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso.', '', { duration: 5000 } );
   }
 }
