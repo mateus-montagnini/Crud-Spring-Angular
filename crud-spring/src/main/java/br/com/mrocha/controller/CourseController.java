c124a3f5e0e8a4c25134c68882f6aff99ef9484f
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-11 10:02:58 -0300

    Validacoes no back-end

--- crud-spring/src/main/java/br/com/mrocha/controller/CourseController.java
+++ crud-spring/src/main/java/br/com/mrocha/controller/CourseController.java
@@ -3,14 +3,20 @@ package br.com.mrocha.controller;
 import br.com.mrocha.model.Course;
 import br.com.mrocha.repository.ICourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 import java.util.Optional;
 
+@Validated
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
@@ -24,21 +30,21 @@ public class CourseController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable("id") Long id) {
+    public ResponseEntity<Course> findById(@PathVariable("id") @NotNull @Positive Long id) {
         return courseRepository.findById(id)
                 .map(record -> ResponseEntity.ok().body(record))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity create(@RequestBody Course course) {
+    public ResponseEntity create(@RequestBody @Valid Course course) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(courseRepository.save(course));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable("id") Long id,
-                                         @RequestBody Course course) {
+    public ResponseEntity<Course> update(@PathVariable("id") @NotNull @Positive Long id,
+                                         @RequestBody @Valid Course course) {
         return courseRepository.findById(id)
                 .map(recordFound -> {
                     recordFound.setName(course.getName());
@@ -50,7 +56,7 @@ public class CourseController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+    public ResponseEntity<Void> delete(@PathVariable("id") @NotNull @Positive Long id) {
         return courseRepository.findById(id)
                 .map(recordFound -> {
                     courseRepository.deleteById(id);
--- crud-spring/src/main/java/br/com/mrocha/model/Course.java
+++ crud-spring/src/main/java/br/com/mrocha/model/Course.java
@@ -3,6 +3,11 @@ package br.com.mrocha.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @Entity
@@ -14,9 +19,14 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
+    @NotBlank
+    @NotNull
     @Column(name = "nome", length = 150, nullable = false)
     private String name;
 
+    @NotBlank
+    @NotNull
+    @Pattern(regexp = "Back-end | Front-end")
     @Column(name = "categoria", length = 100, nullable = false)
     private String category;
 }
