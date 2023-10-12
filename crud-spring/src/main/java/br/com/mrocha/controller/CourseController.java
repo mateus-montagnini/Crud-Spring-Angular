28d1bd44902e725bfd30c3247c1705de38a92b31
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-12 09:28:12 -0300

    Removendo imports

--- crud-spring/src/main/java/br/com/mrocha/controller/CourseController.java
+++ crud-spring/src/main/java/br/com/mrocha/controller/CourseController.java
@@ -3,9 +3,7 @@ package br.com.mrocha.controller;
 import br.com.mrocha.model.Course;
 import br.com.mrocha.repository.ICourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +12,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.util.List;
-import java.util.Optional;
 
 @Validated
 @RestController
