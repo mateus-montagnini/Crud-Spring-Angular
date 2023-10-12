bf89a95f58441f78dff0e740f7f86f14e0e75b9a
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-12 08:57:48 -0300

    Criando coluna de status do curso

--- crud-spring/src/main/java/br/com/mrocha/model/Course.java
+++ crud-spring/src/main/java/br/com/mrocha/model/Course.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.Length;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -11,6 +13,8 @@ import javax.validation.constraints.Pattern;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE tb_curso SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 @Table(name = "tb_curso")
 public class Course {
 
@@ -29,4 +33,11 @@ public class Course {
     @Pattern(regexp = "Back-end | Front-end")
     @Column(name = "categoria", length = 100, nullable = false)
     private String category;
+
+    @NotBlank
+    @NotNull
+    @Pattern(regexp = "Ativo | Inativo")
+    @Column(name = "status", length = 10, nullable = false)
+    private String status;
+
 }
