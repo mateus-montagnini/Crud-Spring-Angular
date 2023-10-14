e272b712c256dcfd00c6c9454052b819d555f460
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-14 13:05:39 -0300

    Atualizando CourseDTO

--- crud-spring/src/main/java/br/com/mrocha/dto/CourseDTO.java
+++ crud-spring/src/main/java/br/com/mrocha/dto/CourseDTO.java
@@ -1,13 +1,16 @@
 package br.com.mrocha.dto;
 
+import br.com.mrocha.model.Lesson;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import java.util.List;
 
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotBlank @NotNull String name,
-        @NotNull @Pattern(regexp = "Back-end | Front-end") String category) {
+        @NotNull @Pattern(regexp = "Back-end | Front-end") String category,
+        List<LessonDTO> lessons) {
 }
