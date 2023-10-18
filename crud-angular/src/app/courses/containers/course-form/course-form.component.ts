5d084b73ff92110fb25c89aa080ef9724aac2326
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-18 06:34:07 -0300

    Validacoes do formulario

--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -28,10 +28,10 @@ export class CourseFormComponent implements OnInit {
     this.form = this.formBuilder.group({
       _id: [course._id],
       name: [course.name, [Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(100)]],
+      Validators.minLength(5),
+      Validators.maxLength(100)]],
       category: [course.category, [Validators.required]],
-      lessons: this.formBuilder.array(this.retrieveLessons(course))
+      lessons: this.formBuilder.array(this.retrieveLessons(course), Validators.required)
     });
   }
 
@@ -48,8 +48,12 @@ export class CourseFormComponent implements OnInit {
   private createLesson(lesson: Lesson = {id: '', name: '', youtubeUrl: ''}) {
     return this.formBuilder.group({
       id: [lesson.id],
-      name: [lesson.name],
-      youtubeUrl: [lesson.youtubeUrl]
+      name: [lesson.name, [Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100)]],
+      youtubeUrl: [lesson.youtubeUrl, [Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(11)]]
     });
   }
 
@@ -68,9 +72,12 @@ export class CourseFormComponent implements OnInit {
   }
 
   onSubmit() {
-    this.service.save(this.form.value)
-    .subscribe(result => this.onSuccess(), error => this.onError());
-
+    if(this.form.valid) {
+      this.service.save(this.form.value)
+        .subscribe(result => this.onSuccess(), error => this.onError());
+    } else {
+      alert('form invalido')
+    }
   }
 
   onCancel() {
@@ -106,5 +113,10 @@ export class CourseFormComponent implements OnInit {
     }
 
     return 'Erro';
+  }
+
+  isFormArrayRequired() {
+    const lessons = this.form.get('lessons') as UntypedFormArray;
+    return !lessons.valid && lessons.hasError('required') && lessons.touched;
   }
 }
