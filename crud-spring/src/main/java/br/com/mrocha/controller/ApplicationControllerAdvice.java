f87f08dea8b3c3cd9ff904be24f7751ccca90255
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-23 12:07:06 -0300

    Corrigindo o ApplicationControllerAdvice

--- crud-spring/src/main/java/br/com/mrocha/controller/ApplicationControllerAdvice.java
+++ crud-spring/src/main/java/br/com/mrocha/controller/ApplicationControllerAdvice.java
@@ -2,9 +2,15 @@ package br.com.mrocha.controller;
 
 import br.com.mrocha.exception.RecordNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -13,5 +19,19 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
     public String handleNotFoundException(RecordNotFoundException ex) {
         return ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public FieldError[] validationError(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        final List<FieldError> fieldErrors = result.getFieldErrors();
+        return fieldErrors.toArray(new FieldError[0]);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleConstraintViolationException(ConstraintViolationException e) {
+        return "not valid due to validation error: " + e.getMessage();
     }
 }
